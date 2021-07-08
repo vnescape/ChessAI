482ca51ea2f183d69a99f06e026b22876a64e79a
vnescape <vnescape@protonmail.com>
2021-07-08 11:42:37 +0200

    Initial code commit

---
+++ Minimax.py
@@ -0,0 +1,105 @@
+import chess
+import chess.svg
+import time
+
+moves = 0
+
+def chessint():
+    if request.method == 'POST':
+        if board_state.turn:
+            uci = request.form['move_post']
+            move = chess.Move.from_uci(uci)
+            print (move)
+            if move in board_state.legal_moves:
+                move = f"{move}"
+                board_state.push_uci(move) #uses UCI
+        else:
+            uci = minimax()
+            move = chess.Move.from_uci(uci)
+            if move in board_state.legal_moves:
+                move = f"{move}"
+                board_state.push_san(move) #uses SAN
+    return render_template('index.html', chessboard=chess.svg.board(board=board_state))
+
+def value(board):
+    value=0
+    pieceValue = {
+        "r":5,"n":3,"b":3,"q":9,"k":999,"p":1, \
+        "R":-5,"N":-3,"B":-3,"Q":-9,"K":-999,"P":-1
+    }
+    fen = list(board.board_fen())
+    for i in range (len(fen)):
+        try:
+            value = value + pieceValue[f"{fen[i]}"] #sum up the values
+        except:
+            pass
+    return value
+
+def legalmoves(board):
+    return repr(board.legal_moves).split("(")[1][:-2] #legalmoves as string
+
+def best_move(board ,depth, alpha, beta, maxplayer):
+    global moves
+    legal = legalmoves(board).split(", ")
+    if depth == 0 or legal[0] == '':
+        return value(board)
+    if maxplayer:
+        value0 = -9999
+        childlist0 = []
+        for c, x in enumerate(legal):
+            board.push_san(x) #Does a move
+            eval_max = minimax(board,depth-1,alpha, beta,False)
+            childlist0.append(eval_max)
+            maxindex0 = childlist0.index(max(childlist0))
+            bestmove0 = legal[maxindex0]
+            alpha = max(alpha, eval_max)
+            board.pop()
+            if alpha >= beta:
+                break #Beta cut-off
+            moves = moves+1
+        return(bestmove0)
+
+def minimax(board ,depth, alpha, beta, maxplayer):
+    global moves
+    legal = legalmoves(board).split(", ")
+    if depth == 0 or board.is_game_over():
+        return value(board)
+    if maxplayer:
+        childlist0 = []
+        for c, x in enumerate(legal):
+            board.push_san(x) #Does a move
+            eval_max = minimax(board,depth-1,alpha, beta,False)
+            alpha = max(alpha, eval_max)
+            board.pop()
+            if alpha >= beta:
+                break #Beta cut-off
+            moves = moves+1
+        return alpha
+   
+    else:
+        childlist1 = []
+        for c, x in enumerate(legal):
+            board.push_san(x) #Does a move
+            eval_min = minimax(board,depth-1, alpha, beta,True)
+            beta = min(beta, eval_min)
+            board.pop()
+            if alpha >= beta:
+                break #Alpha cut-off
+            moves = moves+1
+        return beta
+
+def run(board_state, depth):
+    for i in range(1,depth+1):
+        print("Depth:",i)
+        start = time.time()
+        recommended_move = best_move(board_state,i,-999,999,True)
+        print("Minimax output:",recommended_move)
+        end = time.time()
+        print("Moves:",moves)
+        print("Seconds:",end-start,)
+        print("--------------------------------")
+    return recommended_move
+
+if __name__ == "__main__":
+    state = chess.Board("1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - 0 1")
+    run(state, 2)
---
+++ app.py
@@ -0,0 +1,86 @@
+# import Flask class
+from flask import Flask, render_template, request, Markup
+import chess
+import chess.svg
+import Minimax as ai
+import re
+
+def check_game_state():
+    if board.outcome() is None:
+        return 1
+    elif board.outcome().result() == "1-0":
+        return render_template('note.html', note="You've won!")
+    elif board.outcome().result() == "0-1":
+        return render_template('note.html', note="You've lost!")
+    elif board.outcome().result() == "1/2-1/2":
+        return render_template('note.html', note="Draw!")
+
+# Input sanitization
+def parse_input():
+    if request.form["response"] == "Reset":
+        # magic reset return
+        return 132
+    player_move_raw = request.form["response"]
+    # restrict valid characters
+    player_move = re.sub('[^a-h1-8]', '', player_move_raw.lower())
+    # restrict length of input
+    if len(player_move) > 4:
+        return render_template('note.html', note="Input too long.\
+             Go back and chose one of these moves: " + ai.legalmoves(board))
+    return player_move
+
+def do_user_move(user_input):
+    # check if the game is already over
+    ret = check_game_state()
+    legal = ai.legalmoves(board)
+    # Save return value. Thus only one function call is necessary
+    if ret != 1:
+        return ret
+    try:
+        board.push_san(user_input)
+        return 1
+    except:
+        return render_template('note.html', note="Illegal move. \
+            Go back and chose one of these moves: " + legal)
+
+def do_ai_move():
+    # check if the game is already over
+    ret = check_game_state()
+        # Save return value. Thus only one function call is necessary
+    if ret != 1:
+        return ret
+    ai_move = ai.run(board, depth)
+    board.push_san(ai_move)
+    return 1
+    
+depth = 3
+board = chess.Board()
+
+# create instance of Flask class
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    # I shouldn't be using globals...
+    # It can cause confusion
+    global board
+    if request.method == "GET":
+        return render_template('index.html', svg=Markup(chess.svg.board(board=board)))
+    if request.method == "POST":
+        user_input = parse_input()
+        # check for magic return value of reset button to reset game
+        if user_input == 132:
+            board = chess.Board()
+            return render_template('index.html', svg=Markup(chess.svg.board(board=board)))
+        # Save return value. Thus only one function call is necessary
+        do_user_move_ret = do_user_move(user_input)
+        if do_user_move_ret != 1:
+            return do_user_move_ret
+        # Save return value. Thus only one function call is necessary
+        do_ai_move_ret = do_ai_move()
+        if do_ai_move_ret != 1:
+            return do_ai_move_ret
+        return render_template('index.html', svg=Markup(chess.svg.board(board=board)))
+
+if __name__ == "__main__":
+    app.run(debug=True)
